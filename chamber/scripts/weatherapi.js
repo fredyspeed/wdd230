868dbfe61b027106a1bdad3b1a43ef34a6f2003a
fredyspeed <fredyacademia@gmail.com>
2022-11-21 13:43:15 -0600

    activity about OpenweatherMap

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -1,22 +1,39 @@
 // select HTML elements in the document
+const currentHumidity = document.querySelector('#current-humidity');
+const currentWindSpeed = document.querySelector('#current-wind_speed');
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&APPID=361dfed55b656f30c52e218031a83655';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=361dfed55b656f30c52e218031a83655';
 
 async function apiFetch() {
     try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data); // this is for testing the call
-        // displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data); // this is for testing the call
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
     } catch (error) {
         console.log(error);
     }
-  }
-  
-  apiFetch();
+}
+
+function displayResults(weatherData) {
+    currentHumidity.innerHTML = `<strong>${weatherData.main.humidity.toFixed(0)}</strong>`;
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    currentWindSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(0)}</strong>`;
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+    let desc_capitalize = desc.toUpperCase();
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc_capitalize);
+    captionDesc.textContent = desc_capitalize;
+
+    
+
+}
+
+apiFetch();
