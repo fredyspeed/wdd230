3fbba3ce93d4462b72d86aa03fab5b1eb783c9e0
fredyspeed <fredyacademia@gmail.com>
2022-12-15 00:39:00 -0600

    fix show section

--- bountifulfoods/scripts/drinks_sends.js
+++ bountifulfoods/scripts/drinks_sends.js
@@ -4,7 +4,7 @@ const drinks_send = document.querySelector("#drinks_sends");
 if (!localStorage.getItem("number_drink_send")) {
 	drinks_send.textContent = `you haven't sent drink specials yet `;
 } else {
-	drinks_send.textContent = localStorage.getItem("number_drink_send");
+	drinks_send.innerHTML = 'you haven sent <strong> ' +localStorage.getItem("number_drink_send")+ '</strong>  drink specials';
 }
 
 
